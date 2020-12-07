9ed5b2c179851e6676936bcfc9d577ee084d38f4
Paulo Ferreira <gammalambda333@gmail.com>
2020-12-07 14:28:40 +0000

    Added types to previous solutions

--- day5/day5.py
+++ day5/day5.py
@@ -1,31 +1,37 @@
 from os import path
+from typing import Optional
 import re
 
-def parse_seat_coords(input_file):
+class Coord:
+    def __init__(self, row: int, col: int):
+        self.row = row
+        self.col = col
+
+def parse_seat_coords(input_file: str) -> list[Coord]:
     with open(input_file) as f:
         return [
             parse_seat_coord(coord_code)
             for coord_code
             in f.readlines()]
         
-def parse_seat_coord(coord_code):
-    return {
-        'row': row_code_to_number(coord_code[:7]),
-        'col': col_code_to_number(coord_code[7:])
-    }
+def parse_seat_coord(coord_code: str) -> Coord:
+    return Coord(
+        row = row_code_to_number(coord_code[:7]),
+        col = col_code_to_number(coord_code[7:])
+    )
 
-def row_code_to_number(row_code):
+def row_code_to_number(row_code: str):
     row_binary = row_code.replace('F', '0').replace('B', '1')
     return int(row_binary, 2)
 
-def col_code_to_number(col_code):
+def col_code_to_number(col_code: str):
     col_binary = col_code.replace('L', '0').replace('R', '1')
     return int(col_binary, 2)
 
-def calculate_seat_id(seat_coord):
-    return seat_coord['row'] * 8 + seat_coord['col']
+def calculate_seat_id(seat_coord: Coord):
+    return seat_coord.row * 8 + seat_coord.col
 
-def find_empty_seat(seat_ids):
+def find_empty_seat(seat_ids: list[int]) -> Optional[int]:
     sorted_seats = sorted(seat_ids)
     prev_seat_id = sorted_seats[0]
     for seat_id in sorted_seats[1:]:
@@ -34,7 +40,7 @@ def find_empty_seat(seat_ids):
         else:
             prev_seat_id = seat_id
 
-def solve(input_file):
+def solve(input_file: str):
     print(f'[{input_file}]')
     full_path = path.join(path.dirname(__file__), input_file)
     seat_coords = parse_seat_coords(full_path)
@@ -43,7 +49,7 @@ def solve(input_file):
     print('Part 2 answer:', find_empty_seat(seat_ids))
     print()
 
-def parse_seat_ids(input_file):
+def parse_seat_ids(input_file: str) -> list[int]:
     # seat_id can be taken directly by parsing the full coordinate as binary
     with open(input_file) as f:
         binary_coords = re.sub(
@@ -52,7 +58,7 @@ def parse_seat_ids(input_file):
             f.read())
         return [int(c, 2) for c in binary_coords.splitlines()]
 
-def solve_take2(input_file):
+def solve_take2(input_file: str):
     print(f'[{input_file}] v2')
     full_path = path.join(path.dirname(__file__), input_file)
     seat_ids = parse_seat_ids(full_path)
--- day6/day6.py
+++ day6/day6.py
@@ -2,22 +2,26 @@ from itertools import chain
 from functools import reduce
 from os import path
 
-def parse_answer_groups(input_file):
+def parse_answer_groups(input_file: str) -> list[list[str]]:
     with open(input_file) as f:
         return [
             group.splitlines()
             for group
             in f.read().split('\n\n')]
         
-def get_unique_answers(answer_group):
+def get_unique_answers(answer_group: list[str]) -> set[str]:
     return set(chain.from_iterable(answer_group))
 
-def get_common_answers(answer_group):
-    return reduce(
-        lambda ans1, ans2: set(ans1) & set(ans2),
-        answer_group)
+def get_common_answers(answer_group: list[str]) -> set[str]:
+    if answer_group == []:
+        return set()
+    else:
+        return reduce(
+            lambda ans1, ans2: set(ans1) & set(ans2),
+            answer_group[1:],
+            set(answer_group[0]))
 
-def solve(input_file):
+def solve(input_file: str):
     print(f'[{input_file}]')
     full_path = path.join(path.dirname(__file__), input_file)
     answer_groups = parse_answer_groups(full_path)
