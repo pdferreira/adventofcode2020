7cd85e2959df347a1d13b8b7b5e206273a675848
Paulo Ferreira <gammalambda333@gmail.com>
2020-12-14 21:17:37 +0000

    Solution for day 14: alternative solution for part 2

--- day14/day14.py
+++ day14/day14.py
@@ -47,7 +47,7 @@ def parse_instruction(line: str) -> Instruction:
 
 def initialize_memory(insts: list[Instruction]) -> dict[int, int]:
     memory = dict[int, int]()
-    and_mask = 2 ** 36 - 1
+    and_mask = 1 << 36 - 1
     or_mask = 0
 
     for inst in insts:
@@ -58,11 +58,14 @@ def initialize_memory(insts: list[Instruction]) -> dict[int, int]:
             memory[inst.mem_pos] = inst.value & and_mask | or_mask
         else:
             raise NotImplementedError('Unknown instruction type: ' + inst.__class__.__name__)
-
+    
     return memory
 
-def gen_memory_positions(base_mem_pos: int, mask: str) -> Iterator[int]:
-    base_bits = bin(base_mem_pos)[2:] # take out the 0b prefix
+def gen_memory_positions(base: int, mask: str) -> Iterator[int]:
+    '''
+    Generate memory positions by applying the `mask` to `base` by recursively building bit-strings.
+    '''
+    base_bits = bin(base)[2:] # take out the 0b prefix
     base_bits = '0' * (len(mask) - len(base_bits)) + base_bits # match mask length
     return map(lambda pos: int(pos, 2), gen_memory_positions_rec(base_bits, mask))
 
@@ -81,6 +84,29 @@ def gen_memory_positions_rec(base: str, mask: str) -> Iterator[str]:
             else:
                 raise Exception('Unexpected mask value: ' + mask[0])
 
+
+def gen_memory_positions_v2(base: int, mask: str, mask_idx: int = 0) -> Iterator[int]:
+    '''
+    Generate memory positions by applying the `mask` to `base` as a series of bitwise operations.
+    Not actually faster than v1.
+    '''
+    mask_len = len(mask)
+    if mask_idx >= mask_len:
+        yield base
+    else:
+        one_mask = 1 << (mask_len - 1 - mask_idx) # mask with 1 at the current bit else 0s
+        if mask[mask_idx] == 'X':
+            zero_mask = ((1 << mask_len) - 1) ^ one_mask # mask with 0 at the current bit else 1s
+            yield from gen_memory_positions_v2(base & zero_mask, mask, mask_idx + 1)
+            yield from gen_memory_positions_v2(base | one_mask, mask, mask_idx + 1)
+        elif mask[mask_idx] == '1':
+            yield from gen_memory_positions_v2(base | one_mask, mask, mask_idx + 1)
+        elif mask[mask_idx] == '0':
+            yield from gen_memory_positions_v2(base, mask, mask_idx + 1)
+        else:
+            raise Exception('Unexpected mask vlue: ' + mask[mask_idx])
+
+
 def initialize_memory_v2(insts: list[Instruction]) -> dict[int, int]:
     memory = dict[int, int]()
     raw_mask = '0' * 26
@@ -89,26 +115,60 @@ def initialize_memory_v2(insts: list[Instruction]) -> dict[int, int]:
         if isinstance(inst, UpdateMask):
             raw_mask = inst.raw_mask
         elif isinstance(inst, UpdateMemory):
-            for pos in gen_memory_positions(inst.mem_pos, raw_mask):
+            for pos in gen_memory_positions_v2(inst.mem_pos, raw_mask):
                 memory[pos] = inst.value
         else:
             raise NotImplementedError('Unknown instruction type: ' + inst.__class__.__name__)
 
     return memory
 
-def solve(input_file: str, skip_part2 = False) -> None:
+def optimize_instructions(insts: list[Instruction], *, within_masks_only: bool) -> list[Instruction]:
+    '''
+    Eliminates repeated writes to the same memory location, preserving the last one.
+    If `within_masks_only` is True, preserves the last one for each mask.
+    '''
+    return optimize_instructions_rec(insts, within_masks_only, set())
+
+def optimize_instructions_rec(insts: list[Instruction], within_masks_only: bool, written_mem_pos: set[int]) -> list[Instruction]:
+    if insts == []:
+        return []
+    else:
+        *rest, last = insts
+        if isinstance(last, UpdateMask):
+            if within_masks_only:
+                written_mem_pos = set()
+            
+            optimized_rest = optimize_instructions_rec(rest, within_masks_only, written_mem_pos)
+            optimized_rest.append(last)
+            return optimized_rest
+
+        elif isinstance(last, UpdateMemory):
+            if last.mem_pos in written_mem_pos:
+                return optimize_instructions_rec(rest, within_masks_only, written_mem_pos)
+            else:
+                written_mem_pos.add(last.mem_pos)
+                optimized_rest = optimize_instructions_rec(rest, within_masks_only, written_mem_pos)
+                optimized_rest.append(last)
+                return optimized_rest
+        
+        else:
+            raise NotImplementedError('Unknown instruction type: ' + last.__class__.__name__)
+
+def solve(input_file: str, *, optimize = True, skip_part2 = False) -> None:
     print(f'[{input_file}]')
     full_path = path.join(path.dirname(__file__), input_file)
 
     insts = parse_instructions(full_path)
 
     start_p1 = time.time_ns()
-    memory = initialize_memory(insts)
+    insts_p1 = optimize_instructions(insts, within_masks_only = False) if optimize else insts
+    memory = initialize_memory(insts_p1)
     print('Part 1 answer:', sum(memory.values()), '(took', time.time_ns() - start_p1, 'ns)')
 
     if not skip_part2:
         start_p2 = time.time_ns()
-        memory_v2 = initialize_memory_v2(insts)
+        insts_p2 = optimize_instructions(insts, within_masks_only = True) if optimize else insts
+        memory_v2 = initialize_memory_v2(insts_p2)
         print('Part 2 answer:', sum(memory_v2.values()), '(took', time.time_ns() - start_p2, 'ns)')
     
     print()
@@ -116,4 +176,4 @@ def solve(input_file: str, skip_part2 = False) -> None:
 if __name__ == '__main__':
     solve('example.txt', skip_part2 = True) # too slow on part2
     solve('example2.txt')
-    solve('input.txt')
+    solve('input.txt', optimize = False)
